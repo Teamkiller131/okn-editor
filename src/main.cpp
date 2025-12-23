d5744de7c5ec834ff41e919105d1dca9baf36c83
Teamkiller131 <ws@tkiller.com>
2025-12-23 16:17:57 +0800

    添加了main.cpp与可执行目标

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+// Created by Teamkiller on 2025/12/23.
+//
+
+#include <cstdlib>
+#include <iostream>
+
+int main(int argc, char** argv) {
+    std::cout << "okn-editor-app placeholder. TODO: wire up real editor bootstrap.\n";
+    return EXIT_SUCCESS;
+}
